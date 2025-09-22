8cb546c4a7e043d1d817f9d51a1b0f8d8af38025
[viniinitc] <[vinitcavalcantena@gmail.com]>
2025-09-22 16:30:50 -0300

    Correcao de erros no app library

---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register_view(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("library: home")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", { "form": form})
+
