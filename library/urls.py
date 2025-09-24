6060fcdf86231897fe9e226acb5beca32861cc50
nibrito20 <minorbrito20@gmail.com>
2025-09-24 00:00:48 -0300

    Adiciona funcionalidade de lista de desejos

--- library/urls.py
+++ library/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
+
+    path('adicionar-livro/', views.addbook, name="add"),
+    path('lista-de-desejos/', views.wishlist_view, name="wishlist"),
+    path('lista-de-desejos/adicionar/<int:book_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('lista-de-desejos/remover/<int:book_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+
     path('', views.booklist, name="list"),
-    path('lista-de-desejos', views.wishlist, name="wish"),
-    path('adicionar-livro', views.addbook, name="add"),
 
     path('<slug:slug>', views.bookpage, name="page"),
     
--- library/views.py
+++ library/views.py
@@ -1,30 +1,34 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book, Wishlist
 from django.contrib.auth.decorators import login_required
 # importar usuarios from users.migrations
 
 
 def booklist(request):
     books = Book.objects.all().order_by('-creationdate')
-    return render(request, 'library/booklist.html', { 'books': books})
+    return render(request, 'booklist.html', { 'books': books})
 
 def bookpage(request, slug):
     book = Book.objects.get(slug=slug)
     return render(request, 'library/bookpage.html', { 'book': book})
 
-
-@login_required(login_url="/users/login/")
-def wishlist(request):
-
-   return render(request, 'library/wishlist.html')
-
-#wishes = user.objecs.wished().order_by('-creationdate') isso seria a base para a lista de desejos
-#wishes = 
-#return render(request, 'library/wishlist.html', { 'wishes' : wishes})
-@login_required(login_url="/users/login/")
 def addbook(request):
-    
-    return render(request, 'library/addbook.html')
+    return render(request, 'addbook.html', {})
 
-#add = 
-#return render(request, 'library/addbook.html', { 'add' : add})
+@login_required
+def wishlist_view(request):
+    wishlist_items = Wishlist.objects.filter(user=request.user).order_by('-added_at')
+    return render(request, 'wishlist.html', {'wishlist_items': wishlist_items})
+
+@login_required
+def add_to_wishlist(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if not Wishlist.objects.filter(user=request.user, book=book).exists(): #vê se o livro tá na lista
+        Wishlist.objects.create(user=request.user, book=book)
+    return redirect('library:wishlist')
+
+@login_required
+def remove_from_wishlist(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    Wishlist.objects.filter(user=request.user, book=book).delete()
+    return redirect('wishlist')
