48bf2ce1c985a22df1e1f3521396a37f2b854b57
nibrito20 <minorbrito20@gmail.com>
2025-09-23 23:58:11 -0300

    Adicionando a class da wishlist

--- library/models.py
+++ library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -19,3 +20,11 @@ class Book(models.Model):
     
 class Comment(models.Model):
     pass
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.book.title}'
