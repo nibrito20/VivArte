56b24e1a15d84c927b68c2e0e3acaa441bc8adc5
nibrito20 <minorbrito20@gmail.com>
2025-09-24 00:03:16 -0300

    Corrige TemplateDoesNotExist na página de login

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#URLs de Autenticação Personalizadas do Django
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'library:list'
