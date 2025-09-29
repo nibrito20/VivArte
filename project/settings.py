8ec43220866d243ec34fbfe737ffe2e906a0ad75
[viniinitc] <[vinitcavalcantena@gmail.com]>
2025-09-29 08:40:27 -0300

    ajustando app e middleware

--- project/settings.py
+++ project/settings.py
@@ -73,10 +73,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'library',
     'users',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,7 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
