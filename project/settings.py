b8ca323d9238193d96fe293633e2f27436e75fb3
viniinitc <vinitcavalcantena@gmail.com>
2025-09-29 10:05:14 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
