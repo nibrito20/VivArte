90ab1c137a2ae6b68b314d73949b8b6c7fd86bef
nibrito20 <minorbrito20@gmail.com>
2025-10-02 20:35:08 -0300

    Configurandoo ALLOWED_HOSTS para o PythonAnyware

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ if NOT_PROD:
     DEBUG = True
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'django-insecure-r$vr8h5igry)+lrrolc)7=fif#bpj!&gg1nwj@87sg-3bk0$1f'
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['livraria.pythonanywhere.com']
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -28,7 +28,8 @@ if NOT_PROD:
 else:
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+    #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+    ALLOWED_HOSTS = ['livraria.pythonanywhere.com']
     CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
 
     SECURE_SSL_REDIRECT = \
