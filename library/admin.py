7e2ffa9e9329b26c4026a67f1d4a8f2e1cee8724
arthurmarmed <arthurmarmed1234@gmail.com>
2025-10-19 17:06:32 -0300

    atualizacao do admin com a classe generos

--- library/admin.py
+++ library/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Genre
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Informações Principais', {
+            'fields': ['title', 'details', 'slug', 'banner']
+        }),
+        ('Categorização e Avaliação', {
+            'fields': ['generos', 'aval']
+        }),
+    ]
+    filter_horizontal = ('generos',)
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Genre) 
+admin.site.register(Book, BookAdmin) 
